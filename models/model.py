274e6111ba529d3f9044aabd6c405d850dfee94e
shicnling <shijing609@163.com>
2018-12-14 16:15:38 +0800

    Bone of model.

--- models/model.py
+++ models/model.py
@@ -3,5 +3,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
-# import data.dict as dict
-import models
+
+class basic_model(nn.Module):
+    def __init__(self, config, tgt_spk_size, use_cuda ):
+        super(basic_model, self).__init__()
+
+        # self.speech_model=SPEECH_MODEL(config,)
+        # self.images_model=IMAGES_MODEL(config,)
+        # self.output_model=MERGE_MODEL(config,)
+        self.use_cuda = use_cuda
+        self.tgt_spk_size = tgt_spk_size
+        self.config = config
+        self.loss_for_ss= nn.MSELoss()
+        self.log_softmax = nn.LogSoftmax()
+
+    def forward(self, *input):
+        return None
+
