472069585cacac6261f2f00118308fcae1d7c2a6
shicnling <shijing609@163.com>
2018-12-28 10:41:48 +0800

    draw heatmaps.

--- heatmap.py
+++ heatmap.py
@@ -1,25 +1,23 @@
 #coding=utf8
+import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 def draw_map(data_path,mask_seq):
+    os.rmdir('visions/')
+    os.mkdir('visions/')
     mask_seq=mask_seq[:,:,:,0]
-    # frames,size0,size1=mask_seq.size()
-    f, (ax1, ax2) = plt.subplots(figsize=(6, 4), nrows=2)
-    # cmap用cubehelix map颜色
-    cmap = sns.cubehelix_palette(start=1.5, rot=3, gamma=0.8, as_cmap=True)
-    sns.heatmap(mask_seq[4], linewidths=0.05, ax=ax1, vmax=1, vmin=0, cmap=cmap)
-    ax1.set_title('cubehelix map')
-    ax1.set_xlabel('')
-    ax1.set_xticklabels([])  # 设置x轴图例为空值
-    ax1.set_ylabel('kind')
+    frames=mask_seq.shape[0]
+    for iii in range(frames):
+        f, ax1 = plt.subplots(figsize=(4, 4),nrows=1 )
+        # cmap用cubehelix map颜色
+        # cmap = sns.cubehelix_palette(start=1.5, rot=3, gamma=0.8, as_cmap=True)
+        # sns.heatmap(mask_seq[4], linewidths=0.05, ax=ax1, vmax=1, vmin=0, cmap=cmap)
+        # sns.heatmap(mask_seq[iii], annot=False, linewidths=0.05, ax=ax1, vmax=0.01, vmin=0, cmap='rainbow')
+        sns.heatmap(mask_seq[iii], annot=False, linewidths=0.05, ax=ax1, cmap='rainbow')
+        ax1.set_title('cubehelix map')
+        ax1.set_xlabel('')
+        ax1.set_xticklabels([])  # 设置x轴图例为空值
+        ax1.set_ylabel('kind')
 
-    # cmap用matplotlib colormap
-    sns.heatmap(mask_seq[5], linewidths=0.05, ax=ax2, vmax=1, vmin=0, cmap='rainbow')
-    # rainbow为 matplotlib 的colormap名称
-    ax2.set_title('matplotlib colormap')
-    ax2.set_xlabel('region')
-    ax2.set_ylabel('kind')
-
-    f.savefig('visions/sns_heatmap_normal.jpg', bbox_inches='tight')
-    return None
+        f.savefig('visions/sns_heatmap_normal_{}.jpg'.format(iii+1), bbox_inches='tight')
